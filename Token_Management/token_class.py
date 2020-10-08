8a6e2a12d88f0bed8d826a258534c86d1f410680
François Vergès <fverges@semfionetworks.com>
2020-10-08 14:24:58 -0400

    Update to the token management script

--- Token_Management/token_class.py
+++ Token_Management/token_class.py
@@ -4,30 +4,27 @@ import requests
 
 
 class Token:
-
-    """Mist Token Object"""
+    """MIST TOKEN OBJECT."""
 
     def __init__(self):
-        """
-            Initialized the Mist Token object.
+        """Initialize the Mist Token object.
 
-            Set the MASTER_TOKEN variable:
-                - Retrieve the environment variable called MIST_TOKEN
-                - Make sure the this environ is set on your system
-                    - Run this command on MacOS: export MIST_TOKEN='<your-token-here>'
+        Set the MASTER_TOKEN variable:
+            - Retrieve the environment variable called MIST_TOKEN
+            - Make sure the this environ is set on your system
+                - Run this command on MacOS: export MIST_TOKEN='<your-token-here>'
         """
         self.MASTER_TOKEN = os.environ['MIST_TOKEN']
 
     def get_tmp_token(self):
-        """
-            Creates a Mist token 'on the fly' to be used witin a specific script.
+        """Create a Mist token 'on the fly' to be used witin a specific script.
 
-            The following API call is made to the Mist Cloud to create a new token:
-                POST https://api.mist.com/api/v1/self/apitokens
+        The following API call is made to the Mist Cloud to create a new token:
+            POST https://api.mist.com/api/v1/self/apitokens
 
-            Set the following class variables:
-             - tmp_token_key: temporary token KEY received from Mist Cloud after creation
-             - tmp_token_id: temporary token ID received from Mist Cloud after creation
+        Set the following class variables:
+         - tmp_token_key: temporary token KEY received from Mist Cloud after creation
+         - tmp_token_id: temporary token ID received from Mist Cloud after creation
         """
         api_url = "https://api.mist.com/api/v1/self/apitokens"
         headers = {"Content-Type": "application/json",
@@ -46,12 +43,10 @@ class Token:
             raise
 
     def delete_tmp_token(self):
-        """
-            Deletes the temporary Mist Token stored in self.tmp_token_key
-            using its ID stored in self.tmp_token_id
+        """Delete the temporary Mist Token stored in self.tmp_token_key using its ID stored in self.tmp_token_id.
 
-            The following API call is made to the Mist Cloud to delete the token:
-                DELETE https://api.mist.com/api/v1/self/apitokens/:token_id
+        The following API call is made to the Mist Cloud to delete the token:
+            DELETE https://api.mist.com/api/v1/self/apitokens/:token_id
         """
         if "tmp_token_id" in self.__dict__:
             api_url = f"https://api.mist.com/api/v1/self/apitokens/{self.tmp_token_id}"
@@ -67,11 +62,11 @@ class Token:
             except Exception:
                 raise
         else:
-            raise ValueError(f"tmp_token_id does not exits.")
+            raise ValueError("tmp_token_id does not exits.")
 
 
 # Tests
-# token = Token()
-# token.get_tmp_token()
-# print("--> We would be running our large script here making our API calls using the tmp token.")
-# token.delete_tmp_token()
+token = Token()
+token.get_tmp_token()
+print("--> We would be running our large script here making our API calls using the tmp token.")
+token.delete_tmp_token()
--- Token_Management/windows_token_demo.py
+++ Token_Management/windows_token_demo.py
@@ -1,21 +1,21 @@
-""" 
+"""
 
 This script that demonsrates how to use token_class.py on Windows
 
 Before running the script, you must define a user-level environmental
-variable that contains your Mist API token. This will be used to access 
+variable that contains your Mist API token. This will be used to access
 the Mist cloud and create the required temporary key(s) to perfom
 scripted actions.
 
-The token_class module relies on the presence of an environmental 
+The token_class module relies on the presence of an environmental
 variable called MIST_TOKEN to authorize access to the Mist API. This
-env var must contain the value of a valid token that has been 
+env var must contain the value of a valid token that has been
 previousy created via the Mist API, e.g. using POSTMAN or some other
 API access tool - see the following URL for an example:
 
     https://www.mist.com/documentation/using-postman/).
 
-To create the user env var, use the following command from a 
+To create the user env var, use the following command from a
 command window on a Windows 10 machine:
 
     setx MIST_TOKEN "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
@@ -26,7 +26,7 @@ is now gloablly available to the user account being used:
 
     echo %MIST_TOKEN%
 
-Note that the env var is only available in a NEW command window. The env 
+Note that the env var is only available in a NEW command window. The env
 var is now permanently avaialble to all processes running under the current
 user account. The env var will not be available to other users on the same
 machine.
@@ -36,11 +36,11 @@ window (the env var will still exist, but will have no value):
 
     setx MIST_TOKEN ""
 
-Or, alternatively it may be deleted via the Windows 10 GUI environmental 
+Or, alternatively it may be deleted via the Windows 10 GUI environmental
 variable editing tool: Start > Control Panel > System & Security >
 System > Advanced System Settings > Environmental Variables (User section)
 
-""" 
+"""
 from token_class import Token
 
 # create Token obj
